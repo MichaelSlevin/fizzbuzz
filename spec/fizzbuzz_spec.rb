2a9f8b2230d2fb1d2d7853e56d2c5ed495cab456
Michael Slevin <mikeslevin@gmail.com>
2018-10-23 10:01:22 +0000

    fizzbuzz_to_20 failing test written

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,7 @@
 require 'fizzbuzz'
 
+expected_output = "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n16\n17\nfizz\n19\nbuzz"
+
 describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
@@ -8,9 +10,10 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(5)).to eq 'buzz'
     end 
     it 'returns "fizzbuzz" when passed 15' do
-        expect(fizzbuzz(5)).to eq 'fizzbuzz'
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end 
     it 'returns "7" when passed 7' do
-        expect(fizzbuzz(7)).to eq '7'
+        expect(fizzbuzz(7)).to eq 7
     end 
+    it "returns \"#{expected_output}\" when fizzbuzz_to_20 is called"
 end
